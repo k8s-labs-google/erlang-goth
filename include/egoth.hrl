47cb9f63ba3cf3ee8e6e5b9f5d56bc921e145d40
Will Beebe <wbeebe@google.com>
2020-08-27 16:31:53 -0700

    removing unused record

--- include/egoth.hrl
+++ include/egoth.hrl
@@ -1,15 +1,3 @@
--record(credentials_file, {auth_provider_x509_cert_url    = undefined :: binary()     | undefined,
-    auth_uri      = undefined :: binary()     | undefined,
-    client_email = undefined :: binary()  | undefined,
-    client_id = undefined :: binary() | undefined,
-    client_x509_cert_url = undefined :: binary() | undefined,
-    private_key = undefined :: binary() | undefined,
-    private_key_id = undefined :: binary() | undefined,
-    project_id = undefined :: binary() | undefined,
-    token_uri = undefined :: binary() | undefined,
-    type = undefined :: binary() | undefined
-}).
-
 -record(config, {
     account :: string(),
     scope   :: string(),
